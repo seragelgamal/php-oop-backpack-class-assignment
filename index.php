07664a43d01a9747d11329d15d7922c644a226af
seragelgamal <55516965+seragelgamal@users.noreply.github.com>
2021-12-14 12:36:52 -0700

    Update index.php

--- index.php
+++ index.php
@@ -11,57 +11,66 @@ class Backpack {
   function openBag() {
     if (!$this->open) {
       $this->open = true;
-      echo("The $this->size $this->color backpack is now open");
+      echo ("The $this->size $this->color backpack is now open");
     } else {
-      echo("The $this->size $this->color backpack is already open");
+      echo ("The $this->size $this->color backpack is already open");
     }
   }
 
   function closeBag() {
     if ($this->open) {
       $this->open = false;
-      echo("The $this->size $this->color backpack is now closed");
+      echo ("The $this->size $this->color backpack is now closed");
     } else {
-      echo("The $this->size $this->color backpack is already closed");
+      echo ("The $this->size $this->color backpack is already closed");
     }
   }
 
   function putin($item) {
     if ($this->open) {
       array_push($this->items, $item);
-      echo("The $item has been put in to the $this->size $this->color backpack");
+      echo ("The $item has been put in to the $this->size $this->color backpack");
     } else {
-      echo("The $this->size $this->color backpack is closed, so the $item couldn't be put in");
+      echo ("The $this->size $this->color backpack is closed, so the $item couldn't be put in");
     }
   }
 
   function takeout($item) {
     if ($this->open) {
-      if (!($itemKey = array_search($item, $this->items))) {
-        echo("There is no $item in the $this->size $this->color backpack, so nothing was taken out");
+      if (($itemKey = array_search($item, $this->items)) === false) {
+        echo ("There is no $item in the $this->size $this->color backpack, so nothing was taken out");
       } else {
         unset($this->items[$itemKey]);
         $this->items = array_keys($this->items);
-        echo("The $item has been taken out of the $this->size $this->color backpack");
+        echo ("The $item has been taken out of the $this->size $this->color backpack");
       }
     } else {
-      echo("The $this->size $this->color backpack is closed, so nothing was taken out");
+      echo ("The $this->size $this->color backpack is closed, so nothing was taken out");
     }
   }
 }
 
 $smallBlueBackpack = new Backpack('blue', 'small');
-var_dump($smallBlueBackpack);
+$mediumRedBackpack = new Backpack('red', 'medium');
+$largeGreenBackpack = new Backpack('green', 'large');
+
+$smallBlueBackpack->openBag();
+echo ('<br>');
+
+$smallBlueBackpack->putin('lunch');
+echo ('<br>');
+
+$smallBlueBackpack->putin('jacket');
+echo ('<br>');
+
+$smallBlueBackpack->closeBag();
 echo('<br>');
 
 $smallBlueBackpack->openBag();
+echo ('<br>');
+
+$smallBlueBackpack->takeout('jacket');
+echo ("<br>");
+
+$smallBlueBackpack->closeBag();
 echo('<br>');
-
-// $smallBlueBackpack->closeBag();
-// echo('<br>');
-
-$smallBlueBackpack->putin('lunch');
-echo('<br>');
-
-$smallBlueBackpack->takeout('lunch');
-echo("<br>");
